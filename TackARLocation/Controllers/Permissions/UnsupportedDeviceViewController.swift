4fcefeb3d7222f7a91296520aa2f5fb6c28ad171
Kelvin Kosbab <kelvin@kosbab.com>
2017-11-19 09:56:52 -0700

    Build 3.

---
+++ TackARLocation/Controllers/Permissions/UnsupportedDeviceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UnsupportedDeviceViewController.swift
+//  KozMap
+//
+//  Created by Kelvin Kosbab on 11/19/17.
+//  Copyright © 2017 Kozinga. All rights reserved.
+//
+
+import UIKit
+
+class UnsupportedDeviceViewController : BaseViewController {
+  
+}
